75afefc0ed94238ca84dbfc762606895b3fd94be
Shameem Ahamad <shameem.ahamad@admins-macbook-pro-2.local>
2016-12-03 02:05:49 +0400

    Initial commit

---
+++ GitTest/GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Shameem Ahamad on 12/3/16.
+//  Copyright © 2016 Shameem Ahamad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Shameem Ahamad on 12/3/16.
+//  Copyright © 2016 Shameem Ahamad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

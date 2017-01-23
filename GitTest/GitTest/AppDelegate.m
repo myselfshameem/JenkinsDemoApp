d9ad44ceeb988a6289142789bd8581e2b8f6d486
Shameem Ahamad <Shameem.Ahamad@darkmatter.ae>
2017-01-23 16:38:13 +0400

    Appdelegate

--- GitTest/GitTest/AppDelegate.m
+++ GitTest/GitTest/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     return YES;
-  //This is from Master branch
+  //Merge this kine to Master Branch
 }
 
 
